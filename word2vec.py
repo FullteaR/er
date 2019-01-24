ab20b1a9890cb6c3ae1b387735e8c577741ab567
Rikuta Furuta <rikuta@furutan.com>
2019-01-24 20:15:00 +0900

    fix: tiny things

--- word2vec.py
+++ word2vec.py
@@ -1,8 +1,10 @@
 from gensim.models import word2vec
 import logging
+import os
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 sentences = word2vec.Text8Corpus('./wiki_processed.txt')
 
 model = word2vec.Word2Vec(sentences, size=200, min_count=5, window=20)
-model.save("./wiki.model")
+os.mkdir("model")
+model.save("./model/wiki.model")
